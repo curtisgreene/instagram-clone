d3429cf3c8815e780faf0054c576d7cb4b96ed2c
Curtis <curtis.j.w.greene@gmail.com>
2019-09-19 12:54:19 -0400

    part one complete

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
       <div>
         <Header />
         <div>
-          <Post />
+          <Post nickname="Chris" avatar="https://www.laravelnigeria.com/img/chris.jpg" caption="Moving the community!" image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" />
+          <Post nickname="OG" avatar="https://www.laravelnigeria.com/img/chris.jpg" caption="Holding a mic" image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" />
         </div>
       </div>
     );
