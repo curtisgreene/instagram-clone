b2de7480b5dc42cf09053825f609f941826f11e3
Curtis <curtis.j.w.greene@gmail.com>
2019-09-20 11:33:43 -0400

    in prog

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Post from './components/Post';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3000'
+})
 
 class App extends Component {
   render() {
     return (
+      <ApolloProvider client={client}>
       <div>
         <Header />
-        <div>
+        <section>
           <Post nickname="Chris" avatar="https://www.laravelnigeria.com/img/chris.jpg" caption="Moving the community!" image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" />
           <Post nickname="OG" avatar="https://www.laravelnigeria.com/img/chris.jpg" caption="Holding a mic" image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" />
-        </div>
+        </section>
       </div>
+      </ApolloProvider>
     );
   }
 }
